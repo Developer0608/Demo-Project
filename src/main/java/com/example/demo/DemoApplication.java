76d37dfa0ab7f00076c9d0fff395913ba7ad67f0
azadsingh99 <azadsingh1934278@gmail.com>
2021-08-10 18:26:53 +0530

    Data Displayed

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -13,7 +18,16 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public String hello(){
-		return "Hello World";
+	public List<Student> hello(){
+		return List.of(new Student(
+				1L,
+				"Azad",
+				"azadsingh@gmail.com",
+				LocalDate.of(2000, Month.JANUARY, 2),
+				21
+		));
 	}
+
+
+
 }
